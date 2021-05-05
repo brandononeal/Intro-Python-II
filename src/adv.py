f351ff1e4c3bcad1e285a5a9f9032ce266b4c370
brandononeal <baoneal95@gmail.com>
2021-05-05 13:51:26 -0700

    refactor

--- src/adv.py
+++ src/adv.py
@@ -5,8 +5,7 @@ from item import Item
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east.""", [Item('Sword', 'God\'s mighty weapon'), Item('Shovel', 'The digger supreme')]),
